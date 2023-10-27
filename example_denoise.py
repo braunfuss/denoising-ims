afefc6e7bd4609ebf9ebcf300bd1c2c6f84099b1
Andreas Steinberg <asteinbe@gfz-potsdam.de>
2023-10-27 14:43:25 +0200

    Update example_denoise.py

--- example_denoise.py
+++ example_denoise.py
@@ -1,8 +1,8 @@
 from denoiser.denoise_utils import denoising_stream
 from obspy import read
+# For this to work you need a folder "input", "output_noise" and "output_signal"
 
-
-st = read("../input/*") # read all data (mseed) in folder
+st = read("input/*") # read all data (mseed) in folder
 
 st.merge(method=-1) # merge gaps and overlaps
 st.filter("highpass", freq=0.01, corners=4) # higpass filter for raw data recommended
@@ -14,8 +14,8 @@ st_de = denoising_stream(stream=st, model_filename="Models/IMS_P.h5",
 
 # write out predicted noises
 for tr in st_de[1]:
-        tr.write("../output_noise/test_%s_%s_%s_%s_%s_IMS.mseed" % (tr.stats.station, tr.stats.location, tr.stats.channel, tr.stats.starttime, tr.stats.endtime))
+        tr.write("output_noise/test_%s_%s_%s_%s_%s_IMS.mseed" % (tr.stats.station, tr.stats.location, tr.stats.channel, tr.stats.starttime, tr.stats.endtime))
 
 # write out predicted signals
 for tr in st_de[0]:
-        tr.write("../output_signal/%s_%s_%s_%s__%s.mseed" % (tr.stats.station, tr.stats.location, tr.stats.channel, tr.stats.starttime, tr.stats.endtime))
+        tr.write("output_signal/%s_%s_%s_%s__%s.mseed" % (tr.stats.station, tr.stats.location, tr.stats.channel, tr.stats.starttime, tr.stats.endtime))
